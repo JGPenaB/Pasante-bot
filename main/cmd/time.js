0f9c6ea6a3f138c584a2e64630402c20c478ba4a
José Padrón <jose.padron712@gmail.com>
2018-10-10 22:32:37 -0400

    Prueba para mostrar la fecha y hora con zona horaria de Venezuela -4

--- main/cmd/time.js
+++ main/cmd/time.js
@@ -1,10 +1,33 @@
 function def(cmd, users, bot, channelID, evt){
-	let hora = new Date().toLocaleTimeString();
-	let fecha = new Date().toLocaleDateString();
+	// Funcion para mostrar la fecha correcamente
+	
+	function dateFormat(d) {
+		return d.getDate()+"/"+d.getMonth()+"/"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+	}
+ 
+	let dateNow = new Date();
+	// cogemos la fecha utc
+	let dateUTC = new Date(dateNow.getUTCFullYear(), dateNow.getUTCMonth(), dateNow.getUTCDate(), dateNow.getUTCHours(), dateNow.getUTCMinutes(), dateNow.getUTCSeconds())
+ 
+	// Definimos la diferencia en horas del time zone
+	// Para la diferencia horaria de dos horas y media seria 2.5
+	let tz = -4;
+	// Calculamos los segundos de la zona horaria
+	let seconds = (tz * 60 * 60) * 1000;
+ 
+	let fechaActual = "Fecha actual => " + dateFormat(dateNow);
+	let fechaUTC = "Fecha UTC => " + dateFormat(dateUTC);
+ 
+	// Aplicamos la diferencia horaria añadiendo los segundos al timestamp de la
+	// fecha UTC
+	dateUTC.setTime(dateUTC.getTime() + seconds);
+ 
+	let timeZone = "TimeZone => " + tz;
+	let fechaUTC_timeZone = "Fecha UTC+TimeZone => " + dateFormat(dateUTC);
 
 	bot.sendMessage({
         to: channelID,
-        message: `La fecha actual es: ${fecha} y la hora actual es: ${hora}`
+        message: `${fechaActual}\n${fechaUTC}\n${timeZone}\n${fechaUTC_timeZone}`
     });
 };
 
