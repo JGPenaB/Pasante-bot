8b4ffb45339070ef414f6249ac24a439449a7373
JGPenaB <jesusgpenab@gmail.com>
2018-11-12 18:52:08 -0400

    pequeño error con la version verbose de def

--- main/cmd/define.js
+++ main/cmd/define.js
@@ -1,6 +1,6 @@
 function def(cmd, user, users, bot, channelID, evt){
 	const https = require('https');
-	https.get('https://en.wikipedia.org/w/api.php?action=opensearch&search='+cmd.substring(5)+'&limit=1&namespace=0&format=json', (resp) => {
+	https.get('https://en.wikipedia.org/w/api.php?action=opensearch&search='+cmd.substring(8)+'&limit=1&namespace=0&format=json', (resp) => {
 					let data = '';
 					let exdata = '';
 					let extract = '';
