eae98cc86e55a87ec4dbf960774c109996bfcf3f
José Padrón <jose.padron712@gmail.com>
2018-11-13 12:04:10 -0400

    Actualizacion de !search

--- main/bot.js
+++ main/bot.js
@@ -1,9 +1,9 @@
-const Discord = require('discord.io');
-const dotenv = require('dotenv').config();
-const logger = require('winston');
-const https = require('https');
-const PNGImage = require('pngjs-image');
-const imageDataURI = require('image-data-uri');
+const Discord = require("discord.io");
+const dotenv = require("dotenv").config();
+const logger = require("winston");
+const https = require("https");
+const PNGImage = require("pngjs-image");
+const imageDataURI = require("image-data-uri");
 
 //Prefijo
 const pfix = "!";
@@ -13,49 +13,52 @@ const port = process.env.PORT || 8080;
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console(), {
-    colorize: true
+  colorize: true
 });
 
-logger.level = 'debug';
+logger.level = "debug";
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: process.env.TOKEN,
-   autorun: true
+  token: process.env.TOKEN,
+  autorun: true
 });
 
-bot.on('ready', function (evt) {
-    logger.info('Conectado');
-    logger.info('Conectado como: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+bot.on("ready", function(evt) {
+  logger.info("Conectado");
+  logger.info("Conectado como: ");
+  logger.info(bot.username + " - (" + bot.id + ")");
 });
 
+bot.on("message", function(user, userID, channelID, message, evt) {
+  if (message.substring(0, 1) == pfix) {
+    let args = message.substring(1).split(" ");
+    let cmd = args[0];
+    let nd = bot.servers[bot.channels[channelID].guild_id].members;
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == pfix) {
-		let args = message.substring(1).split(' ');
-        let cmd = args[0];
-		let nd = bot.servers[bot.channels[channelID].guild_id].members;
-		
-		//Extraer el username de los usuarios a un array
-		let users = [];
-		for (let key in nd){
-			if (nd[key].status != undefined && nd[key].status != 'offline' && !(bot.users[key].bot)){ users.push(bot.users[key].username);}
-		}
-		
-		//Carga un archivo que contiene la lógica del comando y ejecuta su función.
-		try{
-			let exec = require("./cmd/"+cmd+".js");
-			exec.def(message, user, users, bot, channelID, evt);
-		}catch(err){
-			console.log(err);
-			bot.sendMessage({
-                to: channelID,
-                message: '¿Perdón? No te entendí muy bien. Intenta esto:```cs\n !ayuda ```'
-            });
-		}
-		
-     }
+    //Extraer el username de los usuarios a un array
+    let users = [];
+    for (let key in nd) {
+      if (
+        nd[key].status != undefined &&
+        nd[key].status != "offline" &&
+        !bot.users[key].bot
+      ) {
+        users.push(bot.users[key].username);
+      }
+    }
+
+    //Carga un archivo que contiene la lógica del comando y ejecuta su función.
+    try {
+      let exec = require("./cmd/" + cmd + ".js");
+      exec.def(message, user, users, bot, channelID, evt);
+    } catch (err) {
+      console.log(err);
+      bot.sendMessage({
+        to: channelID,
+        message:
+          "¿Perdón? No te entendí muy bien. Intenta esto:```cs\n !ayuda ```"
+      });
+    }
+  }
 });
-
-
--- main/cmd/search.js
+++ main/cmd/search.js
@@ -1,80 +1,65 @@
-var request = require("request");
-
-// cmd, users, bot, channelID, evt
-
-// AIzaSyDXRDiDFiVs4kX-O3SZijMmZ6CriEWrbN0
-// AIzaSyAeQz6NFOKJB53Z8LqCXp5x0OxAp36FD14
+const request = require("request");
+const cheerio = require("cheerio");
 
 function def(cmd, user, users, bot, channelID, evt) {
-  const query = cmd.substring(8);
-  // const query = "MiaKhalifa";
-  const key = "AIzaSyAeQz6NFOKJB53Z8LqCXp5x0OxAp36FD14";
-  const cx = "003208579516977437485:krqbsnzmnrw";
-  let url = `https://www.googleapis.com/customsearch/v1?key=${key}&cx=${cx}&q=${query}`;
+  const query = encodeURI(cmd.substring(8));
+  // const query = encodeURI("Mia Khalifa");
+  const url = `https://www.bing.com/images/search?q=${query}&safesearch=off`;
 
-  request(
-    // Primer parametro un objeto con URL, y diciendo que será un JSON
-    {
-      url: url,
-      json: true
-    },
-    function(error, response, body) {
-      if (!error && response.statusCode === 403) {
-        // Si entra acá es porque se agotaron los 100 querys
-        if (body.error) {
+  // Si entra acá todo bien
+  request(url, (err, res, body) => {
+    if (!err && res.statusCode === 200) {
+      let $ = cheerio.load(body);
+      let link = $("img")[2].attribs.src;
+
+      // Quita los parametros que recortan la img
+      link = link.substr(0, 61);
+
+      // Si entra acá todo bien al mandar el msg a discord
+      bot.sendMessage(
+        {
+          to: channelID,
+          message: "Una imagen",
+          embed: {
+            color: 6826080,
+            footer: {
+              text: "Powered by Bing."
+            },
+            image: {
+              url: link
+            }
+          }
+        },
+        function(err, res) {
+          // Si entra acá hubo un error para mandar el msg a discord
           bot.sendMessage({
             to: channelID,
-            message: `Chamo, estaba haciendo eso y la PC se colgó. Que chimbo.`
-          });
+            message: err
+          })
         }
-      }
-
-      if (!error && response.statusCode === 200) {
-        // Si entra acá todo se hizo correctamente
-        // console.log(body); // Print the json response
-        for (var i = 0; i < 1; i++) {
-          let item = body.items[i];
-          let title = item.title;
-          let link = "no image";
-
-          if (item.pagemap.cse_image) {
-            // console.log("cse");
-            link = item.pagemap.cse_image[0].src;
-          } else if (item.pagemap.hcard) {
-            // console.log("hcard");
-            link = item.pagemap.hcard[0].photo;
-          }
-          // console.log(link);
-
-          if (link != "no image") {
-            bot.sendMessage(
-              {
-                to: channelID,
-                message: "Una imagen",
-                embed: {
-                  color: 6826080,
-                  footer: {
-                    text: "Powered by Gugul."
-                  },
-                  image: {
-                    url: link
-                  }
-                }
-              },
-              function(error, response) {
-                console.log(error);
-              }
-            );
-          } else {
-            bot.sendMessage({
-              to: channelID,
-              message: `Mano, no conseguí la imagen. Estaba ocupado con la geva... Tú sabes.`
-            });
-          }
-        }
-      }
+      );
+      // console.log(link);
     }
-  );
+
+    if (err || res.statusCode != 200) {
+      // Si entra acá hubo un error al hacer el request, posible mal URL
+
+      bot.sendMessage({
+        to: channelID,
+        message: "El Autz no sabe programar",
+        embed: {
+          color: 6826080,
+          footer: {
+            text: "Dificultades tecnicas brother"
+          },
+          image: {
+            url: "https://i.ytimg.com/vi/a3rmgGoibsE/maxresdefault.jpg"
+          }
+        }
+      });
+      // console.error(error, response.statusCode);
+    }
+  });
 }
 // def();
 module.exports.def = def;
