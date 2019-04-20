78e27996830e793f792d55bba00c5ff213b9cc7e
JGPenaB <jesusgpenab@gmail.com>
2019-04-20 19:50:33 -0400

    pequeno bugfix

--- main/cmd/so.js
+++ main/cmd/so.js
@@ -16,43 +16,45 @@ function def(cmd, user, users, bot, channelID, evt){
 		let final_data = '';
 		console.log(jsondata_base);
 		
-		if(jsondata_base.items.length > 0 && jsondata_base.items[0].is_answered == true){
-			https.get(jsondata_base.items[0].link, function(resp){
-				resp.on('data', (c) => {
-					final_data += c;
-				});
-		
-				resp.on('end', () =>{
-					let $ = cheerio.load(final_data);
-					let pred = $(".answercell").first().text();
-					let final_text = pred.substring(0,pred.search("share"));
-					
-					//Para evitar errores con el embed
-					if(final_text.length > 1020) final_text=final_text.substring(0,1020)+"...";
-				
-					bot.sendMessage({
-						to: channelID,
-						message: 'Esto fue lo primero que encontré en Stack Overflow:',
-						embed:{    
-							color: 16749596,	
-							title: jsondata_base.items[0].title,
-							url: jsondata_base.items[0].link,
-							fields: [
-								{
-									name: "Respuesta",
-									value: final_text.trim()
-								}
-							],
-						
-						}
-					}, function(error, response){console.log(error);});
-				});
-			});
-		}else if(!jsondata_base.items[0].is_answered){
+		if(jsondata_base.items.length > 0){
+			
+			if(!jsondata_base.items[0].is_answered){
 			bot.sendMessage({
 				to: channelID,
 				message: "Encontré una pregunta, pero no posee una respuesta definitiva. Puedes revisarlo si te interesa: \n"+jsondata_base.items[0].link
 			});
+			}else{
+				https.get(jsondata_base.items[0].link, function(resp){
+					resp.on('data', (c) => {
+						final_data += c;
+					});
+		
+					resp.on('end', () =>{
+						let $ = cheerio.load(final_data);
+						let pred = $(".answercell").first().text();
+						let final_text = pred.substring(0,pred.search("share"));
+					
+						//Para evitar errores con el embed
+						if(final_text.length > 1020) final_text=final_text.substring(0,1020)+"...";
+				
+						bot.sendMessage({
+							to: channelID,
+							message: 'Esto fue lo primero que encontré en Stack Overflow:',
+							embed:{    
+								color: 16749596,	
+								title: jsondata_base.items[0].title,
+								url: jsondata_base.items[0].link,
+								fields: [
+									{
+										name: "Respuesta",
+										value: final_text.trim()
+									}
+								],
+							}
+						}, function(error, response){console.log(error);});
+					});
+				});
+			}
 		}else{
 			bot.sendMessage({
 				to: channelID,
