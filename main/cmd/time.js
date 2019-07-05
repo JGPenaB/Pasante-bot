a428a79091a7072d42afa5a45283b8304c0ffdf6
Nicolás Giacaman <nicolas.egp@gmail.com>
2019-07-05 18:57:39 -0400

    Fix para el PR Quality :(

--- main/cmd/time.js
+++ main/cmd/time.js
@@ -1,29 +1,29 @@
 function def(cmd, user, users, bot, channelID, evt) {
-	var Fecha = new Date()
+	var Fecha = new Date();
 	bot.sendMessage({
-		'to': channelID,
-		'embed': {
-			'color': 700605,
-			'fields': [
+		"to": channelID,
+		"embed": {
+			"color": 700605,
+			"fields": [
 				{
-					'name': ':flag_ve: Venezuela',
-					'value': Fecha.toLocaleString('es-ES', {timeZone: 'America/Caracas'})
+					"name": ":flag_ve: Venezuela",
+					"value": Fecha.toLocaleString("es-ES", {timeZone: "America/Caracas"})
 				},
 				{
 					'name': ':flag_co: Colombia',
-					'value': Fecha.toLocaleString('es-ES', {timeZone: 'America/Bogota'})
+					"value": Fecha.toLocaleString("es-ES", {timeZone: "America/Bogota"})
 				},
 				{
 					'name': ':flag_cl: Chile',
-					'value': Fecha.toLocaleString('es-ES', {timeZone: 'America/Santiago'})
+					"value": Fecha.toLocaleString("es-ES", {timeZone: "America/Santiago"})
 				},
 				{
 					'name': ':flag_do: República Dominicana',
-					'value': Fecha.toLocaleString('es-ES', {timeZone: 'America/Santo_Domingo'})
+					"value": Fecha.toLocaleString("es-ES", {timeZone: "America/Santo_Domingo"})
 				}
 			]
 		}
-	})
+	});
 }
 
-module.exports.def = def
+module.exports.def = def;
