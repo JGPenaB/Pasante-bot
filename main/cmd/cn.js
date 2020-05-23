cbca02aa354c2bf3b151d1b09d16025d864081ab
JGPenaB <jesusgpenab@gmail.com>
2020-05-23 18:09:24 -0400

    probando unos detalles

--- main/cmd/cn.js
+++ main/cmd/cn.js
@@ -1,9 +1,9 @@
-const axios = require('axios');
+const axios = require("axios");
 
 Number.prototype.moneda = function () {
 	const re = '\\d(?=(\\d{3})+\\D)',
 		num = this.toFixed(Math.max(2));
-	return (num.replace('.', ',')).replace(new RegExp(re, 'g'), '$&.')
+	return (num.replace('.', ',')).replace(new RegExp(re, 'g'), '$&.');
 };
 
 function def(cmd, user, users, bot, channelID, evt) {
@@ -13,31 +13,31 @@ function def(cmd, user, users, bot, channelID, evt) {
 	let cantidad = parseFloat(message[2]);
 	const Sitios = [
         //axios.get('https://s3.amazonaws.com/dolartoday/data.json')
-        axios.get('https://monitordolar.com/api/index.php?action=ver')
+        axios.get("https://monitordolar.com/api/index.php?action=ver")
     ];
 
 	if ((modo === "D" || modo === "B") && !isNaN(cantidad)) {
 
 		axios.all(Sitios).then(axios.spread((dolar) => {
 			var DolarToday = dolar.data.actual.dolarToday;
-        	var AirTM = dolar.data.actual.airTM;
+			var AirTM = dolar.data.actual.airTM;
 			var promedio = dolar.data.actual.promedioTotal;
 			var titulo = "";
 			var cambio = [];
-			var desde = "$"
-			var hasta = "VES"
+			var desde = "$";
+			var hasta = "VES";
 			
 			if (modo === "D"){
 				titulo = "Cambio de USD a VES";
-				desde = "$"
-			    hasta = "VES"
+				desde = "$";
+				hasta = "VES";
 				cambio.push(cantidad*DolarToday);
 				cambio.push(cantidad*AirTM);
 				cambio.push(cantidad*promedio);
 			} else if (modo === "B"){
 				titulo = "Cambio de VES a USD";
-				desde = "VES"
-			    hasta = "$"
+				desde = "VES";
+				hasta = "$";
 				cambio.push(cantidad/DolarToday);
 				cambio.push(cantidad/AirTM);
 				cambio.push(cantidad/promedio);
--- main/cmd/dolar.js
+++ main/cmd/dolar.js
@@ -1,15 +1,15 @@
-const axios = require('axios');
+const axios = require("axios");
 
 Number.prototype.moneda = function () {
-    const re = '\\d(?=(\\d{3})+\\D)',
+    const re = "\\d(?=(\\d{3})+\\D)",
         num = this.toFixed(Math.max(2));
-    return (num.replace('.', ',')).replace(new RegExp(re, 'g'), '$&.')
+    return (num.replace(".", ",")).replace(new RegExp(re, "g"), "$&.")
 };
 
 function def(cmd, user, users, bot, channelID, evt) {
     const Sitios = [
-        //axios.get('https://s3.amazonaws.com/dolartoday/data.json')
-        axios.get('https://monitordolar.com/api/index.php?action=ver')
+        //axios.get("https://s3.amazonaws.com/dolartoday/data.json")
+        axios.get("https://monitordolar.com/api/index.php?action=ver")
     ];
 
     axios.all(Sitios).then(axios.spread((dolar) => {
@@ -19,45 +19,45 @@ function def(cmd, user, users, bot, channelID, evt) {
         var bolCucuta = dolar.data.actual.bolivarCucuta;
         var promedio = dolar.data.actual.promedioTotal;
         var Fields = []
-        if (DolarToday == null || AirTM == null || LocalBit == null || bolCucuta == null || promedio == null) {
+        if (DolarToday === null && AirTM === null && LocalBit === null && bolCucuta === null && promedio === null) {
             Fields.push({
-                'name': 'Error',
-                'value': '**Sin datos**'
-            })
+                "name": "Error",
+                "value": "**Sin datos**"
+            });
         } else {
             Fields.push({
-                'name': 'DolarToday',
-                'value': '**' + Number(DolarToday).moneda() + ' VES**',
-                'inline': false
-            })
+                "name": "DolarToday",
+                "value": `**${Number(DolarToday).moneda()} VES**`,
+                "inline": false
+            });
             Fields.push({
-                'name': 'AirTM',
-                'value': '**' + Number(AirTM).moneda() + ' VES**',
-                'inline': false
-            })
+                "name": "AirTM",
+                "value": `**${Number(AirTM).moneda()} VES**`,
+                "inline": false
+            });
             Fields.push({
-                'name': 'LocalBitcoin',
-                'value': '**' + Number(LocalBit).moneda() + ' VES**',
-                'inline': false
-            })
+                "name": "LocalBitcoin",
+                "value": `**${Number(LocalBit).moneda()} VES**`,
+                "inline": false
+            });
             Fields.push({
-                'name': 'Bolívar Cúcuta',
-                'value': '**' + Number(bolCucuta).moneda() + ' VES**',
-                'inline': false
-            })
+                "name": "Bolívar Cúcuta",
+                "value": `**${Number(bolCucuta).moneda()} VES**`,
+                "inline": false
+            });
             Fields.push({
-                'name': 'Promedio',
-                'value': '**' + Number(promedio).moneda() + ' VES**',
-                'inline': false
-            })
+                "name": "Promedio",
+                "value": `**${Number(promedio).moneda()} VES**`,
+                "inline": false
+            });
         }
         bot.sendMessage({
-            'to': channelID,
-            'message': 'Mano, tuve que usar VPN y todo para ver esta vaina:',
-            'embed': {
-                'color': 2264407,
-                'title': 'Tasa de conversión actual',
-                'fields': Fields
+            "to": channelID,
+            "message": "Mano, tuve que usar VPN y todo para ver esta vaina:",
+            "embed": {
+                "color": 2264407,
+                "title": "Tasa de conversión actual",
+                "fields": Fields
             }
         }, (error, response) => {
 
@@ -65,8 +65,8 @@ function def(cmd, user, users, bot, channelID, evt) {
                 console.log(error);
 
                 bot.sendMessage({
-                    'to': channelID,
-                    'message': 'Mano, no sé que pasa. El Internet Explorer dice que me quedé sin Internet.',
+                    "to": channelID,
+                    "message": "Mano, no sé que pasa. El Internet Explorer dice que me quedé sin Internet.",
                 });
             }
         });
