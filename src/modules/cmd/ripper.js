384cc6935299214bf87db0104e07fb34999bbb43
José Padrón <jose.padron712@gmail.com>
2021-01-29 22:58:13 -0400

    Tranquilos hijos mios, pronto acomodaré ripper (protip: no lo haré :pepekek:)

--- src/modules/cmd/ripper.js
+++ src/modules/cmd/ripper.js
@@ -5,6 +5,7 @@ const zipFolder = require('zip-a-folder');
 const rimraf = require('rimraf');
 const { exec } = require('child_process');
 const { Message } = require('discord.js');
+const http = require("http");
 
 /**
  * Información sobre el comando
@@ -38,6 +39,7 @@ const main = async (message, userName) => {
     const prefix = process.env.PREFIX;
     const args = message.content.substring(prefix.length).split(' ');
     const url = args[1];
+    var info;
 
     function recursiveDownload (urlArray, nameArray, i, path) {
         if (i < urlArray.length) {
@@ -61,17 +63,112 @@ const main = async (message, userName) => {
                 if(err) {
                     console.error('Error al comprimir la carpeta rippeada: ' + err);
                 } else {
+                    console.log('Subiendo archivo a File.io');
                     message.channel.send('Subiendo a File.io');
 
                     exec(`curl -F "file=@${path}.zip" https://file.io`, (err, stdout, stderr) => {
                         if (err) {
                             console.error('Error subiendo el archivo a file.io' + err)
                         } else {
-                            console.log(`stdout: ${stdout}`);
-
                             const link = JSON.parse(stdout);
+                            console.log(`file.io upload result: ${link.success}`);
 
-                            message.channel.send(`${link.link}`);
+                            if (link.success == false) {
+                                rimraf.sync(path);
+                                fs.unlinkSync(path+".zip"); 
+
+                                return message.channel.send(`Hubo un malpario error subiendo tu mierda a file.io`);
+                            }
+
+                            html = `
+                            <html>
+                                <head>
+                                    <style>
+                                        div.gallery {
+                                        margin: 5px;
+                                        border: 1px solid #ccc;
+                                        float: left;
+                                        width: 180px;
+                                    }
+
+                                    div.gallery:hover {
+                                        border: 1px solid #777;
+                                    }
+
+                                    div.gallery img {
+                                        width: 100%;
+                                        height: auto;
+                                    }
+
+                                    div.desc {
+                                        padding: 15px;
+                                        text-align: center;
+                                    }
+                                    </style>
+                                </head>
+
+                                <body>
+                                    <h3>Descargando desde <a target='_blank' href='https://${info.host}/${info.tablon}/res/${info.hilo}.html'>${info.host}/${info.tablon}/${info.hilo}</a></h3>
+                                    <a target='_blank' href='${link.link}'>Descargar zip</a><br>
+                            `;
+
+                            src.forEach(el => {
+                                html += `
+                                    <div class="gallery">
+                                        <a target="_blank" href="${el}">
+                                            <img src="${el}" alt="chanero" width="600" height="400">
+                                        </a>
+                                    </div>
+                                `;
+                            });
+
+                            html += `
+                                </body>
+                            </html>
+                            `;
+
+
+
+                            // se crea el servidor http
+                            const requestListener = function (req, res) {
+                                res.writeHead(200, {'Content-Type': 'text/html'});
+                                res.end(html);
+                                server.close();
+                            };
+                            
+                            const server = http.createServer(requestListener);
+
+                            const host = process.env.HOST_ADDR || 'localhost';
+                            const port = process.env.HOST_PORT || 8000;
+                
+                            server.listen(port, host, () => {
+                                message.channel.send(`Descarga tu mierda en http://${host}:${port}`);
+                            });
+                
+                            const sockets = new Set();
+                
+                            server.on('connection', (socket) => {
+                                sockets.add(socket);
+                
+                                server.once('close', () => {
+                                    sockets.delete(socket);
+                                });
+                            });
+                
+                            /**
+                             * Muerte al http server.
+                             */
+                            const close = (callback) => {
+                                for (const socket of sockets) {
+                                    socket.destroy();
+                
+                                    sockets.delete(socket);
+                                }
+                
+                                server.close(callback);
+                            };
+
+                            //message.channel.send(`${link.link}`);
 
                             rimraf.sync(path);
                             fs.unlinkSync(path+".zip"); 
@@ -91,7 +188,7 @@ const main = async (message, userName) => {
     } else {
         const partes = url.split("/");
 
-        const info = {
+        info = {
             host: partes[2],
             tablon: partes[3],
             hilo: partes[5].split(".")[0]
