af4f9bfa7a85c9eab429343734bbfe7334f4dab7
leo polanco <leo.polanco.n@gmail.com>
2021-03-09 19:53:00 -0400

    Mejora del comando stock

--- src/modules/cmd/stock.js
+++ src/modules/cmd/stock.js
@@ -1,5 +1,8 @@
 const { Message } = require('discord.js')
 const axios = require('axios')
+const random = require('../../utils/random')
+const messages = require('../messages/stock')
+// let randomAvatar = images[random.num(images.length)];
 
 /**
  * Información sobre el comando
@@ -27,20 +30,53 @@ const help = () => ({
 const main = async (message) => {
   let query = message.content.substring(7).replace(/\s/g,'')
 
-  if(query.length<1) {
-    return message.channel.send('¿Que es lo que voy a buscar? Sacate el webo de la boca')
+  if( query.length<1 ) {
+    return message.channel.send('Ingresa el simbolo de tu stock favorita, TSLA para Tesla')
   }
-
   try {
-    const { data: { symbol, latestPrice } } = await axios.get(`https://cloud.iexapis.com/stable/stock/${query}/quote`, {
+    const { data:{quote , news} } = await axios.get(`https://cloud.iexapis.com/stable/stock/${query}/batch?types=quote,news`, {
       params: {
         token: process.env.IEX_KEY
       }
     })
-   return message.channel.send(`Rescata, ${symbol} anda ahorita en $${latestPrice}`)
+    
+    //Extraemos las primeras 2 noticias en ingles
+    const englishNews = news
+    .filter((newsItem)=>newsItem.lang==='en')
+    .map((newsItem)=>`[${newsItem.headline}](${newsItem.url})`)
+    .slice(0,2)
+    
+    let signo = quote.change > 0 ? '+' : ''
+    const embedData = {
+      color: quote.changePercent > 0 ? 3141900 : 16711680,
+      title: quote.companyName,
+      fields: [
+       { 
+         name: 'Precio',
+         value:`$${quote.latestPrice}`
+       },
+       { 
+         name:'Cambio %',
+         value: `${signo}${quote.change}  ( ${signo}${Math.round(quote.changePercent*100)/100}% )`
+       },
+       { 
+         name:'Volumen',
+         value: `${quote.avgTotalVolume}`
+       },
+       {
+         name:'Noticias',
+         value:englishNews,
+       }
+      ],
+      footer: { text: `Bolsa: ${quote.isUSMarketOpen ? 'abierta' : 'cerrada'}` }
+    }
+
+   return message.channel.send(`${messages[random.num(messages.length)]} ${quote.symbol}`, {
+     embed: embedData
+   })
   } catch (err) {
     console.log(err.message)
-    return  message.channel.send(`El mio no te encontre na, acuerdate que es el symbol. \nPor ejemplo AAPL para conseguirte apple`)
+    return  message.channel.send(`El mio no te encontre na, acuerdate que es el simbolo. \nEjemplo AAPL para rescatarte apple`)
   }
 }
 
