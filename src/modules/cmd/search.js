2e0e72567c2ba97b437eccbede91c4c9a4881803
leo polanco <leo.polanco.n@gmail.com>
2021-03-10 00:58:31 -0400

    acomodando cmd search

--- src/modules/cmd/search.js
+++ src/modules/cmd/search.js
@@ -1,79 +1,78 @@
-const { Message } = require('discord.js');
-const request = require('request');
-const cheerio = require('cheerio');
+const { Message } = require('discord.js')
+const axios = require('axios')
+const cheerio = require('cheerio')
 
-const messages = require('../messages/search');
+const random = require('../../utils/random')
+const messages = require('../messages/search')
 
 /**
  * Lista de alias válidos para el comando
- * 
+ *
  * @return { Array<string> }
  */
-const aliases = () => {
-    return ['search'];
-};
+const aliases = () => ['search']
 
 /**
  * Información sobre el comando
- * 
+ *
  * @return { Object }
  */
-const help = () => {
-    return {
-        "usage": "!search {query}",
-        "desc": "Busca una imagen en Bing usando un query, y lo postea en el chat. El SafeSearch está habilitado para los canales corrientes.",
-        "example": "Si busco una imagen de Venezuela:\n!search Venezuela\n\nSi busco una película como Toy Story:\n!search toy story"
-    }
-};
+const help = () => ({
+  usage: '!search {query}',
+  desc:
+    'Busca una imagen en Bing usando un query, y lo postea en el chat. El SafeSearch está habilitado para los canales corrientes.',
+  example:
+    'Si busco una imagen de Venezuela:\n!search Venezuela\n\nSi busco una película como Toy Story:\n!search toy story'
+})
 
 /**
  * Manejador del comando
- * 
+ *
  * @param { Message } message Evento completo del mensaje
  */
 const main = async (message) => {
-    const query = encodeURI(message.content.substring(8));
-    let url = `https://www.bing.com/images/search?q=${query}`;
+  const query = encodeURI(message.content.substring(8))
+  let url = `https://www.bing.com/images/search?q=${query}`
 
-    if (message.channel.nsfw === true) {
-        url += '&safesearch=off';
-    }
+  if (message.channel.nsfw === true) {
+    url += '&safesearch=off'
+  }
 
-    const answer = messages[Math.floor(Math.random() * messages.length)];
+  let imageLink = 'https://i.ytimg.com/vi/a3rmgGoibsE/maxresdefault.jpg'
 
-    request(url, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-            return message.channel.send({
-                embed: {
-                    color: 5396735,
-                    footer: {
-                        text: 'Dificultades técnicas brother'
-                    },
-                    image: {
-                        url: 'https://i.ytimg.com/vi/a3rmgGoibsE/maxresdefault.jpg'
-                    }
-                }
-            });
+  try {
+    const { data } = await axios.get(url)
+    const $ = cheerio.load(data)
+    imageLink = $('img')[11].attribs.src
+    // Quita los parámetros que recortan la img
+    imageLink = imageLink.substr(0, 61)
+
+    return message.channel.send(messages[random.num(messages.length)], {
+      embed: {
+        color: 5396735,
+        footer: {
+          text: 'Powered by Bing.'
+        },
+        image: {
+          url: imageLink
         }
+      }
+    })
+  } catch (err) {
+    console.log(err.message)
 
-        const $ = cheerio.load(body);
-        let link = $('img')[11].attribs.src;
+    return message.channel.send({
+      embed: {
+        color: 5396735,
+        footer: {
+          text: 'Dificultades técnicas brother'
+        },
+        image: {
+          url: imageLink
+        }
+      }
+    })
+  }
+}
 
-        // Quita los parámetros que recortan la img
-        link = link.substr(0, 61);
-
-        message.channel.send(answer, {
-            embed: {
-                color: 5396735,
-                footer: {
-                    text: 'Powered by Bing.'
-                },
-                image: {
-                    url: link
-                }
-            }
-        })
-    });
-};
-
-module.exports = { aliases, help, main };
+module.exports = { aliases, help, main }
