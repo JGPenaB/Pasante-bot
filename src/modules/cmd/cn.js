2df860af32fb202ffc7db6410f78c7d99c6ab3ee
Jose Padron <jose.padron712@gmail.com>
2022-02-19 12:15:37 -0400

    Se da soporte para pesos chilenos en cmd cn

--- src/modules/cmd/cn.js
+++ src/modules/cmd/cn.js
@@ -1,6 +1,8 @@
 const { Message } = require('discord.js');
 
 const dolarService = require('../../services/dolarService');
+const chileanPesoService = require('../../services/chileanPesoService');
+const { formatNumber } = require('../../utils/numbers');
 
 /**
  * Lista de alias válidos para el comando
@@ -15,10 +17,10 @@ const aliases = () => ['cambio', 'cn', 'cmb'];
  * @return { Object }
  */
 const help = () => ({
-  usage: '!cambio {B o D} {monto}',
-  desc: 'Convierte USD a VED (o vice versa) usando distintas tasas de cambio.',
+  usage: '!cambio {B o D o P} {monto}',
+  desc: 'Convierte USD o CLP o VED a USD/CLP/VED (sin incluir moneda origen) usando distintas tasas de cambio.',
   example:
-    'Si quiero convertir $10 a Bolívares Soberanos (VED):\n!cambio d 10\n\nSi quiero convertir 500 Bolívares Soberanos a Dólar (USD):\n!cambio b 500'
+    'Si quiero convertir 10 Dolares Americanos a VED y CLP:\n!cambio d 10\n\nSi quiero convertir 500 Bolívares Soberanos a USD y CLP:\n!cambio b 500\n\nSi quiero convertir 800 Pesos Chilenos a USD y VED:\n!cambio p 800'
 });
 
 /**
@@ -29,59 +31,124 @@ const help = () => ({
 const main = async (message) => {
   const prefix = process.env.PREFIX;
   const args = message.content.substring(prefix.length).split(' ');
-  const mode = args[1];
+  const mode = args[1] && typeof args[1] === 'string' ? args[1].toUpperCase() : null;
   const amount = parseFloat(args[2]);
+  let title = null;
+  const exchange = [];
 
-  if (mode === undefined || !(mode.toUpperCase() === 'B' || mode.toUpperCase() === 'D') || isNaN(amount)) {
+  if (mode === null || !(mode === 'B' || mode === 'D' || mode === 'P') || isNaN(amount)) {
     return message.channel.send(
       'Mano, no puedo hacer la conversión si faltan datos o los datos que me das están malos.'
     );
   }
 
-  let title = 'Cambio de USD a VED';
-  let from = '$';
-  let to = 'VED';
-  const exchange = [];
-
-  const exchangeRates = await dolarService.getExchangeRates().catch(() => {
+  const exchangeRatesB = await dolarService.getExchangeRates(true).catch(() => {
     message.channel.send('MonitorDolar dejó de funcionar... O me bloquearon, no sé.');
   });
 
-  const dolarToday = exchangeRates[0].value;
-  const airTM = exchangeRates[3].value;
+  const exchangeRatesP = await chileanPesoService.getExchangeRates().catch(() => {
+    message.channel.send('API tasas de cambio CLP dejó de funcionar... O me bloquearon, no sé.');
+  });
 
-  if (mode.toUpperCase() === 'D') {
-    exchange.push(amount * dolarToday);
-    exchange.push(amount * airTM);
-  } else {
-    title = 'Cambio de VED a USD';
-    from = 'VED';
-    to = '$';
+  const exchangeRates = exchangeRatesB.concat(exchangeRatesP);
 
-    exchange.push(amount / dolarToday);
-    exchange.push(amount / airTM);
+  switch (mode) {
+    case 'B':
+      title = `Cambio de VED a USD y CLP`;
+
+      exchangeRates.forEach(exchangeRate => {
+        let result = 0, resultFrom = '', resultTo = '';
+
+        switch (exchangeRate.nationalCurrency) {
+          case 'CLP':
+            resultFrom = 'VED';
+            resultTo = 'CLP';
+            // 1ro hay que pasar los bolos a USD, cristo perdoname por quemar valores
+            result = (amount / exchangeRatesB[0].value) * exchangeRate.value;
+            break;
+
+          case 'VED':
+            resultFrom = 'VED';
+            resultTo = 'USD';
+            result = amount / exchangeRate.value;
+            break;
+        }
+
+        exchange.push({
+          name: `Tasa ${exchangeRate.title} (${exchangeRate.rate} ${exchangeRate.nationalCurrency}):`,
+          value: `**${resultFrom} ${formatNumber(amount)}** => **${resultTo} ${formatNumber(result)}**`
+        });
+      });
+      break;
+
+    case 'D':
+      title = `Cambio de USD a VED y CLP`;
+
+      exchangeRates.forEach(exchangeRate => {
+        let result = 0, resultFrom = '', resultTo = '';
+
+        switch (exchangeRate.nationalCurrency) {
+          case 'CLP':
+            resultFrom = 'USD';
+            resultTo = 'CLP';
+            result = amount * exchangeRate.value;
+            break;
+
+          case 'VED':
+            resultFrom = 'USD';
+            resultTo = 'VED';
+            result = amount * exchangeRate.value;
+            break;
+        }
+
+        exchange.push({
+          name: `Tasa ${exchangeRate.title} (${exchangeRate.rate} ${exchangeRate.nationalCurrency}):`,
+          value: `**${resultFrom} ${formatNumber(amount)}** => **${resultTo} ${formatNumber(result)}**`
+        });
+      });
+      break;
+
+    case 'P':
+      title = `Cambio de CLP a USD y VED`;
+
+      exchangeRates.forEach(exchangeRate => {
+        let result = 0, resultFrom = '', resultTo = '';
+
+        switch (exchangeRate.nationalCurrency) {
+          case 'CLP':
+            resultFrom = 'CLP';
+            resultTo = 'USD';
+            result = amount / exchangeRate.value;
+            break;
+
+          case 'VED':
+            resultFrom = 'CLP';
+            resultTo = 'VED';
+            // 1ro hay que pasar los pesos a USD luego multiplicarlo por la tasa de USD->bolos, cristo perdoname por quemar valores
+            result = amount / exchangeRatesP[0].value;
+            result = result * exchangeRate.value;
+            break;
+        }
+
+        exchange.push({
+          name: `Tasa ${exchangeRate.title} (${exchangeRate.rate} ${exchangeRate.nationalCurrency}):`,
+          value: `**${resultFrom} ${formatNumber(amount)}** => **${resultTo} ${formatNumber(result)}**`
+        });
+      });
+      break;
   }
 
-  console.log('EXCHANGE:::', exchange);
+  if (exchange.length === 0) {
+    return message.channel.send(
+      'Mano no me cargo NI UNA tasa de cambio, que ladilla NOJODA.'
+    );
+  }
 
   message.channel.send('Tuve que usar una calculadora, porque esto es demasiada matemática para mí', {
     embed: {
       color: 3141900,
       title,
-      fields: [
-        {
-          name: `Tasa DolarToday (${dolarService.formatNumber(dolarToday)} VED):`,
-          value: `**${from} ${dolarService.formatNumber(amount)}** => **${dolarService.formatNumber(
-            exchange[0]
-          )} ${to}**`
-        },
-        {
-          name: `Tasa AirTM (${dolarService.formatNumber(airTM)} VED):`,
-          value: `**${from} ${dolarService.formatNumber(amount)}** => **${dolarService.formatNumber(
-            exchange[1]
-          )} ${to}**`
-        }
-      ]
+      fields: exchange
     }
   });
 };
