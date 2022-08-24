21664761838d8414f114f6f37b36f7cd8962ff91
JGPenaB <jesusgpenab@gmail.com>
2022-08-24 15:17:40 -0400

    arreglado scraping the monitordolarvenezuela

--- src/services/dolarService.js
+++ src/services/dolarService.js
@@ -11,16 +11,18 @@ const getExchangeRates = async (useRegex = false) => {
   const list = [];
   const regexTitles = new RegExp(/^(dolar today|airtm)$/, 'i');
 
-  await axios.get(`https://monitordolarvenezuela.com`).then((dolar) => {
+  await axios.get(`https://monitordolarvenezuela.com/inicio-amp`, 
+  { headers: {
+    "User-Agent": 
+    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Mobile Safari/537.36"
+    }
+  }).then((dolar) => {
     let $ = cheerio.load(dolar.data);
 
-    //Extrae el valor desde el CSS, ya que usan pseudo-elementos
-    const regexp = /content:(\s+)\'([^\s]+)\'/g;
-    const matches = [...dolar.data.matchAll(regexp)];
-
+    //Extrae el valor desde la version mobile del sitio, que no la han actualizado
     $('.box-prices').each((index, el) => {
-      let title = $('div', el).first().text();
-      let price = matches[index][2];
+      let title = $("div", el).first().text();
+      let price = $("div", el).first().next().text();
       let shouldPush = true;
 
       price = price.replace(/\./g, '').replace(',', '.');
