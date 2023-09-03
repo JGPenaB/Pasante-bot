5a0c802ffea4daafafa4bbb9d9ca3d1daefa1a82
JGPenaB <jesusgpenab@gmail.com>
2023-09-03 14:53:40 +0200

    MonitorDolar nunca ganará

--- src/services/dolarService.js
+++ src/services/dolarService.js
@@ -1,5 +1,6 @@
-const axios = require('axios');
+//const axios = require('axios');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const { formatNumber } = require('../utils/numbers');
 
 /**
@@ -11,30 +12,38 @@ const getExchangeRates = async (useRegex = false) => {
   const list = [];
   const regexTitles = new RegExp(/(dolar\s{0,}today|bcv|airtm|binance)/, 'gi');
 
-  await axios.get(`https://monitordolarvenezuela.com/`).then((dolar) => {
-    let $ = cheerio.load(dolar.data);
+  //MonitorDolar nunca ganará
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://monitordolarvenezuela.com/');
+  await page.waitForSelector('h3', { timeout: 5000 });
 
-    //MonitorDolar nunca ganará
-    $('h3').each((index, el) => {
-      let title = $(el).first().text();
-      let price = $(el).first().next().next().next().text();
-     
-      let shouldPush = true;
+  const body = await page.evaluate(() => {
+    return document.querySelector('html').innerHTML;
+  });
 
-      price = price.replace(/\S{1,}\s=\s{1,}/i, '');
-      price = price.replace(/\./g, '').replace(',', '.');
+  let $ = cheerio.load(body);
 
-      title = title.replace('@','');
-      
-      if (useRegex && !regexTitles.test(title)) {
-        shouldPush = false
-      }
+  //MonitorDolar nunca ganará
+  $('h3').each((index, el) => {
+    let title = $(el).first().text();
+    let price = $(el).first().next().next().next().text();
+    
+    let shouldPush = true;
 
-      // Guarda el "valor" convertido para realizar operaciones matemáticas
-      if (price && !isNaN(price) && shouldPush) {
-        list.push({ title, rate: formatNumber(Number(price)), value: price, nationalCurrency: 'VED' })
-      };
-    });
+    price = price.replace(/\S{1,}\s=\s{1,}/i, '');
+    price = price.replace(/\./g, '').replace(',', '.');
+
+    title = title.replace('@','');
+    
+    if (useRegex && !regexTitles.test(title)) {
+      shouldPush = false
+    }
+
+    // Guarda el "valor" convertido para realizar operaciones matemáticas
+    if (price && !isNaN(price) && shouldPush) {
+      list.push({ title, rate: formatNumber(Number(price)), value: price, nationalCurrency: 'VED' })
+    };
   });
 
   return list;
