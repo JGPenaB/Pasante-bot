38acd638efb0b6d7ad39b45164bad4d20c69c9c0
JGPenaB <jesusgpenab@gmail.com>
2024-08-27 23:45:41 +0200

    added ai commant stub

---
+++ src/modules/cmd/ai.js
@@ -0,0 +1,31 @@
+const { Message } = require('discord.js');
+
+/**
+ * Lista de alias válidos para el comando
+ *
+ * @return { Array<string> }
+ */
+const aliases = () => ['ai'];
+
+/**
+ * Información sobre el comando
+ *
+ * @return { Object }
+ */
+const help = () => ({
+  usage: '!ai {query}',
+  desc: 'test.',
+  example: 'test'
+});
+
+/**
+ * Manejador del comando
+ *
+ * @param { Message } message Evento completo del mensaje
+ * @param { string } userName Nombre del usuario que triggereó el evento
+ */
+const main = (message, userName) => {
+  return message.channel.send(`test`);
+};
+
+module.exports = { aliases, help, main };
